ac90f80a91c389410608fa333dd1e0cc09e3742b
Sean Tseng <latte0205@gmail.com>
2020-11-07 01:30:57 +0800

    Day07

---
+++ Day07.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+# Step1. 首先讓我們創建個空箱子，把箱子內的資料印出來，並且用type()將資料型別印出
+box = 9487
+print(box)
+type(box)
+
+
+# In[4]:
+
+
+# Step2. 首先讓我們創建個空箱子，把箱子內的資料印出來，並且用type()將資料型別印出
+box1 = "9487 94狂"
+print(box1)
+type(box1)
+
+
+# In[5]:
+
+
+# Step3. 首先讓我們創建兩個空箱子，然後把兩個箱子相加的數值印出來，並且用type()將資料型別印出
+box1 = 9487
+box2 = 10000
+add = box1+box2
+print(add)
+type(add)
+
+
+# In[7]:
+
+
+# Step4. 首先讓我們創建兩個空箱子，然後把兩個箱子相加的數值印出來，並且用type()將資料型別印出(此題示範為字串str)
+box1 = 'Hello, '
+box2 = 'Sean.'
+add = box1+box2
+print(add)
+type(add)
+
+
+# In[8]:
+
+
+# Step5. 假設我們創建兩個空箱子，其一箱子為整數，另一個為字串，若相加會如何呢？
+box1 = 1
+box2 = 'Sean.'
+add = box1+box2
+print(add)
+type(add)
+
+
+# In[ ]:
+
+
+
+
